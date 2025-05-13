99d294928033cd68734b8c2381a184151b0e7ef4
Marcos Felipe <p.marcosfelipe@gmail.com>
2025-05-13 20:12:59 +0100

    create pix payment method

---
+++ payments/pix.py
@@ -0,0 +1,19 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self):
+        pass
+
+    def create_payment(self):
+        # creates payment in the financial institution
+        bank_payment_id = uuid.uuid4()
+
+        # qr code
+        hash_payment = f'has_payment_{bank_payment_id}'
+        qrcode_image = qrcode.make(hash_payment)
+
+        # save qr code as image
+        qrcode_image.save(f"static/img/qrcode_payment_{bank_payment_id}.png")
+
+        return { "bank_payment_id" : bank_payment_id, "qr_code_path" : f"qrcode_payment_{bank_payment_id}"}
