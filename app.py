88e41e4a29085f22925330dbc2dad93faeae0d65
Marcos Felipe <p.marcosfelipe@gmail.com>
2025-05-29 08:26:01 +0100

    add validation on payment confirmation route

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ from payments.pix import Pix
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
 
 db.init_app(app)
 socketio = SocketIO(app)
 
-@app.route('/payments/pix', methods=['POST'])
+
+@app.route("/payments/pix", methods=["POST"])
 def create_payment_pix():
     data = request.get_json()
-    
-    if 'value' not in data:
-        return jsonify({'message': 'Invalid value!'}), 400
-    
+
+    if "value" not in data:
+        return jsonify({"message": "Invalid value!"}), 400
+
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data["value"], expiration_date=expiration_date)
@@ -31,28 +32,56 @@ def create_payment_pix():
     db.session.add(new_payment)
     db.session.commit()
 
-    return jsonify({'message': 'The payment has been created!', 'payment': new_payment.to_dict()})
+    return jsonify(
+        {"message": "The payment has been created!", "payment": new_payment.to_dict()}
+    )
 
 
-@app.route('/payments/pix/qrcode/<file_name>', methods=['GET'])
+@app.route("/payments/pix/qrcode/<file_name>", methods=["GET"])
 def get_qrcode_image(file_name):
-    return send_file(f'static/img/{file_name}.png', mimetype='image/png')
+    return send_file(f"static/img/{file_name}.png", mimetype="image/png")
 
-@app.route('/payments/pix/confirmation', methods=['POST'])
+
+@app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
-    return jsonify({'message': 'The payment has been confirmed!'})
+    data = request.get_json()
 
-@app.route('/payments/pix/<int:payment_id>', methods=['GET'])
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data!"}), 400
+
+    payment = Payment.query.filter_by(
+        bank_payment_id=data.get("bank_payment_id")
+    ).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found!"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data!"}), 400
+
+    payment.paid = True
+    db.session.commit()
+
+    return jsonify({"message": "The payment has been confirmed!"})
+
+
+@app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
-    return render_template('payment.html', payment_id=payment.id, value=payment.value, host='http://127.0.0.1:5000', qr_code=payment.qr_code)
+    return render_template(
+        "payment.html",
+        payment_id=payment.id,
+        value=payment.value,
+        host="http://127.0.0.1:5000",
+        qr_code=payment.qr_code,
+    )
 
 
-@socketio.on('connect')
+@socketio.on("connect")
 def handle_connect():
-    print('client connected to the server...')
+    print("client connected to the server...")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, debug=True)
